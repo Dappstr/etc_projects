69154735a4d2250ea5fee430062bb293a643dc13
Lane Beals <lanebeals02@gmail.com>
2023-05-06 19:01:29 -0700

    Added code to main.rs

--- binary_to_ascii/rust/src/main.rs
+++ binary_to_ascii/rust/src/main.rs
@@ -3,6 +3,37 @@ use std::fs::File;
 use std::env;
 use std::io::Result;
 
+fn exponent(val: i32, exp: i32) -> i32 {
+    let mut result:i32 = 1;
+    let mut i: i32 = 0;
+    
+    while i < exp {
+        result *= val;
+        i += 1;
+    }
+    result
+}
+
+fn binary_to_decimal(arr: [bool; 8]) -> i32 {
+    let mut decimal_value: i32 = 0;
+    let mut indx: usize = 0;
+    
+    while indx < 8 {
+        decimal_value += (arr[indx] as i32) * (exponent(2, 7 - (indx as i32)));
+        indx += 1;
+    }
+    decimal_value
+}
+
+fn reset_binary(arr: &mut [bool; 8]) {
+    let mut indx: usize = 0;
+    while indx < 8 {
+        arr[indx] = false;
+        indx += 1;
+    }
+}
+
+
 fn main() -> Result<()>{
     let args: Vec<String> = env::args().collect();
 
@@ -21,6 +52,8 @@ fn main() -> Result<()>{
     
     file.read_to_string(&mut content)?;
     println!("File contents: {}", content);
+    
+    print!("{}", exponent(4, 3));
 
     Ok(())
 }
