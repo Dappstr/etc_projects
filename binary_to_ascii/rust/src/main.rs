3aa66b16d4c3f024f7c9ead51044ea72fe64094d
Lane Beals <lanebeals02@gmail.com>
2023-05-06 17:30:55 -0700

    Change to Rust main file

--- binary_to_ascii/rust/src/main.rs
+++ binary_to_ascii/rust/src/main.rs
@@ -1,7 +1,26 @@
-use std::io;
+use std::io::Read;
 use std::fs::File;
 use std::env;
+use std::io::Result;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()>{
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+            println!("Too many arguments!\n");
+            std::process::exit(1);
+        }
+    else if args.len() < 2 {
+          println!("Too few arguments!\n");
+          std::process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let mut file = File::open(file_path)?;
+    let mut content = String::new();
+    
+    file.read_to_string(&mut content)?;
+    println!("File contents: {}", content);
+
+    Ok(())
 }
